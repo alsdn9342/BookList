6087ec53c9e9bdde9b35fdf0567117bb2091c507
min woo <alsdn9342@gmail.com>
2021-08-05 14:25:14 +0900

    Connect MS SQL and create database with Book table

---
+++ Model/Book.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key] //Primary Key
+        public int Id { get; set; }
+       
+        [Required] // Not allowed null
+        public string Name { get; set; }
+     
+        public string Author { get; set; }
+
+    }
+}
