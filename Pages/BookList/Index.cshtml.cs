38ffc0fa1d2c727b81c006f8a7315c4c50f1dea8
min woo <alsdn9342@gmail.com>
2021-08-05 16:36:15 +0900

    Add Get Habdler

---
+++ Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using BookListRazor.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BookListRazor.Model.ApplicationDbContext _db;
+
+        public IndexModel(BookListRazor.Model.ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();           
+        }
+
+    }
+}
