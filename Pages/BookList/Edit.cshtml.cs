213b8327b65558ea0c789569ac6e311430e91215
min woo <alsdn9342@gmail.com>
2021-08-07 11:35:03 +0900

    Add Edit Page

---
+++ Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookListRazor.Model;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+
+        }
+    
+    }
+       
+}
